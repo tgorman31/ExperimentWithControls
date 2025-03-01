9aeacdf2e92b7dad12c76ee2b620abcfe7cd37d1
tgorman31 <gorman.thomas31@gmail.com>
2025-03-01 14:05:54 +1300

    Chapter 2 complete with multiple different control types and how they interact

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,6 +7,15 @@
         public MainPage()
         {
             InitializeComponent();
+
+            BirdPicker.ItemsSource = new string[]
+            {
+                "Duck",
+                "Pigeon",
+                "Penguin",
+                "Ostrich",
+                "Owl"
+            };
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +28,30 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnteredText.Text = e.NewTextValue;
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            StepperValue.Text = e.NewValue.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValue.Text = e.NewValue.ToString();
+        }
+
+        private void AddBird_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Birds.Text))
+            {
+                Birds.Text = Birds.Text + Environment.NewLine;
+            }
+            Birds.Text += BirdPicker.SelectedItem;
         }
     }
 
